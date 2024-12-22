854a59e4f87c39d63fb02a13990e664f834fb60d
Merna-Bahaa <mernabahaa84@gmail.com>
2024-12-22 14:41:06 +0200

    Update single principales.py

--- single principales.py
+++ single principales.py
@@ -1,22 +1,25 @@
+pathlib import Path
+from zipfile import ZipFile
+
 class FileManager:
-
-    def __init__ (self, filename):
-
-        self.path= Path(filename)
+    def __init__(self, filename):
+        self.path = Path(filename)
 
     def read(self, encoding="utf-8"):
-
         return self.path.read_text(encoding)
 
     def write(self, data, encoding="utf-8"):
-
         self.path.write_text(data, encoding)
 
-    def compress(self):
 
+class FileCompressor:
+    def __init__(self, filepath):
+        self.path = Path(filepath)
+
+    def compress(self):
         with ZipFile(self.path.with_suffix(".zip"), mode="w") as archive:
             archive.write(self.path)
 
     def decompress(self):
-        with ZipFile(self.path.with_suffix(".zip"), node -"r") as archive:
-            archive .extractall()
+        with ZipFile(self.path.with_suffix(".zip"), mode="r") as archive:
+            archive.extractall()
